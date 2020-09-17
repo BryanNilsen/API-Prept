72362782ce78fcfc00d750bc274a36b5ac7124ec
BryanNilsen <bryannilsen@gmail.com>
2020-09-16 21:21:22 -0500

    create food added

--- preptrestapi/views/food.py
+++ preptrestapi/views/food.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
-from preptrestapi.models import Food
+from preptrestapi.models import Food, Profile
 
 
 class FoodSerializer(serializers.HyperlinkedModelSerializer):
@@ -19,6 +19,27 @@ class FoodSerializer(serializers.HyperlinkedModelSerializer):
 
 class FoodViewSet(ViewSet):
     """Food resource"""
+
+    def create(self, request):
+        """
+        Handle POST Operations
+        Returns:
+            Response -- JSON serialized Food instance
+        """
+        newfood = Food()
+        newfood.name = request.data["name"]
+        newfood.brand = request.data["brand"]
+        newfood.quantity = request.data["quantity"]
+        newfood.ounces = request.data["ounces"]
+        newfood.servings = request.data["servings"]
+        newfood.calories_per_serving = request.data["calories_per_serving"]
+        newfood.container = request.data["container"]
+        newfood.expiration_date = request.data["expiration_date"]
+        newfood.profile_id = request.data["profile_id"]
+        newfood.save()
+
+        serializer = FoodSerializer(newfood, context={'request': request})
+        return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
         """Handle GET requests for single food item instance
